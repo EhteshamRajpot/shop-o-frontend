b92892eeaef8ee6780d496a1877e2ec08ebe6445
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-01-31 00:07:16 +0500

    first commit

--- src/pages/Shop/ShopSettingsPage.tsx
+++ src/pages/Shop/ShopSettingsPage.tsx
@@ -1,22 +1,21 @@
 import React from "react";
-import Footer from "../../components/Layout/Footer.tsx";
+import { loadSeller } from "../../redux/actions/user.tsx";
 import ShopSettings from "../../components/Shop/ShopSettings.tsx";
 import DashboardHeader from "../../components/Shop/Layout/DashboardHeader.tsx";
 import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar.tsx";
-import { loadSeller } from "../../redux/actions/user.tsx";
 
 const ShopSettingsPage = () => {
-  return (
-    <div>
-      <DashboardHeader />
-      <div className="flex items-start justify-between w-full">
-        <div className="w-[80px] 800px:w-[330px]">
-          <DashboardSideBar active={11} />
+    return (
+        <div>
+            <DashboardHeader />
+            <div className="flex items-start justify-between w-full">
+                <div className="w-[80px] 800px:w-[330px]">
+                    <DashboardSideBar active={11} />
+                </div>
+                <ShopSettings loadSeller={loadSeller} />
+            </div>
         </div>
-        <ShopSettings loadSeller={loadSeller}/>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ShopSettingsPage;
