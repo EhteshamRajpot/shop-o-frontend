90055ed31b7dad902bf36f84bac076657d6dc80f
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-02-01 22:59:45 +0500

    first message

--- src/components/Shop/DashboardMessages.tsx
+++ src/components/Shop/DashboardMessages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { backend_url, server } from '../../server'
 import axios from 'axios'
@@ -6,22 +6,234 @@ import { useNavigate } from 'react-router-dom'
 import { AiOutlineArrowRight, AiOutlineSend } from 'react-icons/ai'
 import styles from '../../styles/styles'
 import { TfiGallery } from "react-icons/tfi";
+import socketIO from "socket.io-client";
+const ENDPOINT = "http://localhost:4000/";
+const socketId = socketIO(ENDPOINT, { transports: ["websocket"] })
 
 const DashboardMessages = () => {
   const { user } = useSelector((state: any) => state.user)
   const { seller } = useSelector((state: any) => state.seller)
   const [conversations, setConversations] = useState<any[]>([]);
-  const [open, setOpen] = useState(false)
+  const [arrivalMessage, setArrivalMessage] = useState<{
+    sender: any;
+    text: any;
+    createdAt: number;
+  } | null>(null);
+  const [currentChat, setCurrentChat] = useState(null)
+  const [messages, setMessages] = useState(null)
+  const [userData, setUserData] = useState(null);
+  const [newMessage, setNewMessage] = useState("")
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [activeStatus, setActiveStatus] = useState(false);
+  const [images, setImages] = useState();
+  const [open, setOpen] = useState(false);
+  const scrollRef = useRef(null);
 
   useEffect(() => {
-    axios.get(`${server}/conversation/get-all-conversation-seller/${seller?._id}`, {
-      withCredentials: true
-    }).then((res) => {
-      setConversations(res.data.conversations)
-    }).catch((error) => {
-      console.log(error)
+    socketId.on("getMessage", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now()
+      })
     })
-  }, [seller])
+  }, [])
+
+
+  useEffect(() => {
+    arrivalMessage &&
+      currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages((prev) => [...prev, arrivalMessage])
+  }, [arrivalMessage, currentChat])
+
+  useEffect(() => {
+    const getConversation = async () => {
+      try {
+        const resonse = await axios.get(
+          `${server}/conversation/get-all-conversation-seller/${seller?._id}`,
+          {
+            withCredentials: true,
+          }
+        );
+
+        setConversations(resonse.data.conversations);
+      } catch (error) {
+        // console.log(error);
+      }
+    };
+    getConversation();
+  }, [seller, messages]);
+
+  useEffect(() => {
+    if (seller) {
+      const sellerId = seller?._id;
+      socketId.emit("addUser", sellerId);
+      socketId.on("getUsers", (data) => {
+        setOnlineUsers(data);
+      });
+    }
+  }, [seller]);
+
+  const onlineCheck = (chat: any) => {
+    const chatMembers = chat.members.find((member) => member !== seller?._id);
+    const online = onlineUsers.find((user) => user.userId === chatMembers);
+
+    return online ? true : false;
+  };
+
+
+  // get messages
+  useEffect(() => {
+    const getMessage = async () => {
+      try {
+        const response = await axios.get(
+          `${server}/message/get-all-messages/${currentChat?._id}`
+        );
+        setMessages(response.data.messages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMessage();
+  }, [currentChat]);
+
+  // create new message
+  const sendMessageHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!currentChat) {
+      // Handle the case when currentChat is null
+      console.error("currentChat is null");
+      return;
+    }
+
+    const message = {
+      sender: seller._id,
+      text: newMessage,
+      conversationId: currentChat._id,
+    };
+
+    const receiverId = currentChat.members.find(
+      (member: any) => member.id !== seller._id
+    );
+
+    socketId.emit("sendMessage", {
+      senderId: seller._id,
+      receiverId,
+      text: newMessage,
+    });
+
+    try {
+      if (newMessage !== "") {
+        await axios
+          .post(`${server}/message/create-new-message`, message)
+          .then((res) => {
+            setMessages([...messages, res.data.message]);
+            updateLastMessage();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateLastMessage = async () => {
+    socketId.emit("updateLastMessage", {
+      lastMessage: newMessage,
+      lastMessageId: seller._id,
+    });
+
+    if (!currentChat) {
+      // Handle the case when currentChat is null
+      console.error("currentChat is null");
+      return;
+    }
+
+    await axios
+      .put(`${server}/conversation/update-last-message/${currentChat._id}`, {
+        lastMessage: newMessage,
+        lastMessageId: seller._id,
+      })
+      .then((res) => {
+        console.log(res.data.conversation);
+        setNewMessage("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleImageUpload = async (e) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImages(reader.result);
+        imageSendingHandler(reader.result);
+      }
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
+  const imageSendingHandler = async (e: React.FormEvent<HTMLImageElement>) => {
+    if (!currentChat) {
+      // Handle the case when currentChat is null
+      console.error("currentChat is null");
+      return;
+    }
+    const receiverId = currentChat.members.find(
+      (member: any) => member !== seller._id
+    );
+
+    socketId.emit("sendMessage", {
+      senderId: seller._id,
+      receiverId,
+      images: e,
+    });
+
+    try {
+      await axios
+        .post(`${server}/message/create-new-message`, {
+          images: e,
+          sender: seller._id,
+          text: newMessage,
+          conversationId: currentChat._id,
+        })
+        .then((res) => {
+          setImages();
+          setMessages([...messages, res.data.message]);
+          updateLastMessageForImage();
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateLastMessageForImage = async () => {
+
+    if (!currentChat) {
+      // Handle the case when currentChat is null
+      console.error("currentChat is null");
+      return;
+    }
+
+    await axios.put(
+      `${server}/conversation/update-last-message/${currentChat._id}`,
+      {
+        lastMessage: "Photo",
+        lastMessageId: seller._id,
+      }
+    );
+  };
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ beahaviour: "smooth" });
+  }, [messages]);
+
 
   return (
     <div className="w-[90%] bg-white m-5 h-[85vh] overflow-y-scroll rounded">
@@ -34,7 +246,7 @@ const DashboardMessages = () => {
             </h1>
             {
               conversations && conversations.map((item, index) => (
-                <MessageList user={user} data={item} key={index} index={index} setOpen={setOpen} />
+                <MessageList user={user} data={item} key={index} index={index} setOpen={setOpen} setCurrentChat={setCurrentChat} />
               ))
             }
           </>
@@ -42,7 +254,13 @@ const DashboardMessages = () => {
 
       {
         open && (
-          <SellerInbox user={user} setOpen={setOpen} />
+          <SellerInbox
+            user={user}
+            setOpen={setOpen}
+            newMessage={newMessage}
+            setNewMessage={setNewMessage}
+            sendMessageHandler={sendMessageHandler}
+          />
         )
       }
     </div>
@@ -54,8 +272,9 @@ interface MessageListProps {
   data: any
   index: any
   setOpen: any
+  setCurrentChat: any
 }
-const MessageList: React.FC<MessageListProps> = ({ user, data, index, setOpen }) => {
+const MessageList: React.FC<MessageListProps> = ({ user, data, index, setOpen, setCurrentChat }) => {
   const [active, setActive] = useState<number>(0)
   const navigate = useNavigate()
   const handleClick = (id: any) => {
@@ -65,8 +284,11 @@ const MessageList: React.FC<MessageListProps> = ({ user, data, index, setOpen })
   return (
     <>
       <div className={`w-full flex p-1 px-3 ${active === index ? "bg-[#00000010]" : "bg-transparent"} cursor-pointer`}
-        // onClick={(e) => setActive(index) || handleClick(data._id)}
-        onClick={(e) => handleClick(data._id)}
+        onClick={(e) => {
+          setActive(index);
+          handleClick(data._id);
+          setCurrentChat(data);
+        }}
       >
         <div className='relative'>
           <img
@@ -88,8 +310,11 @@ const MessageList: React.FC<MessageListProps> = ({ user, data, index, setOpen })
 interface SellerInboxProps {
   user: any
   setOpen: any
+  newMessage: any
+  setNewMessage: any
+  sendMessageHandler: any
 }
-const SellerInbox: React.FC<SellerInboxProps> = ({ user, setOpen }) => {
+const SellerInbox: React.FC<SellerInboxProps> = ({ user, setOpen, newMessage, setNewMessage, sendMessageHandler }) => {
   return (
     <div className="w-full min-h-full flex flex-col justify-between">
       {/* message header */}
@@ -124,13 +349,13 @@ const SellerInbox: React.FC<SellerInboxProps> = ({ user, setOpen }) => {
           <div className='w-max p-2 rounded bg-[#38c776] text-[#fff] h-min'>
             <p>Hi!</p>
           </div>
-        </div> 
+        </div>
       </div>
       {/* send message input */}
       <form
         aria-required={true}
         className="p-3 relative w-full flex justify-between items-center"
-      // onSubmit={sendMessageHandler}
+        onSubmit={sendMessageHandler}
       >
         <div className="w-[30px]">
           <input
@@ -149,8 +374,8 @@ const SellerInbox: React.FC<SellerInboxProps> = ({ user, setOpen }) => {
             type="text"
             required
             placeholder="Enter your message..."
-            // value={newMessage}
-            // onChange={(e) => setNewMessage(e.target.value)}
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
             className={`${styles.input}`}
           />
           <input type="submit" value="Send" className="hidden" id="send" />
