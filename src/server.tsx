739f531769f82e202c944417c694ec7f1da98a52
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-03-28 12:07:52 +0500

    first commit

--- src/server.tsx
+++ src/server.tsx
@@ -1,3 +1,3 @@
-export const server = "https://shop-o-backend-lime.vercel.app/api/v2";
+export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "https://shop-o-backend-lime.vercel.app/";
+export const backend_url = "http://localhost:8000/";
